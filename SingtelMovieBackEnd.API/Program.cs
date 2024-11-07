eeec62ff84f338323f8adffa7c19353747307708
JeyarasaPuvilathan <jp21puvi@gmail.com>
2024-11-07 18:18:08 +0800

    test2

--- SingtelMovieBackEnd.API/Program.cs
+++ SingtelMovieBackEnd.API/Program.cs
@@ -34,18 +34,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
